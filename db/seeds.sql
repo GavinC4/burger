81cc7eac1fc7f4ab7a32006ebeb97f37ef9e27bb
Gavin Clay <cgavin40@yahoo.com>
2017-10-29 21:11:20 -0400

    initial heroku push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Seed Burgers into Database
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2017-10-29 04:20:00"),
+  ("Whoper", false, "2017-10-29 04:20:01"),
+  ("Cheeseburger", false, "2017-10-29 04:20:02"),
+  ("Baconator", false, "2017-10-29 16:20:00"),
+  ("Quarter Pounder", false, "2017-10-29 16:20:01"),
+  ("McRoyal", false, "2017-10-29 04:20:20"),
+  ("Double Double", true, "2017-10-29 16:20:16");
